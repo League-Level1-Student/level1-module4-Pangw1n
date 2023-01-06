5d708a0013a5c4314eee664a5357f6f8fe8872fb
league <league@imac-22.attlocal.net>
2023-01-05 19:56:39 -0800

    Finished:ChuckleClicker;WorkingOn:Jeopardy

--- src/_02_jeopardy/Jeopardy.java
+++ src/_02_jeopardy/Jeopardy.java
@@ -49,19 +49,19 @@ public class Jeopardy implements ActionListener {
 		frame.setLayout(new BorderLayout());
 
 		// 1. Make the frame show up
-
+		frame.setVisible(true);
 		// 2. Give your frame a title
-
+		frame.setTitle("JEOPARDY");
 		// 3. Create a JPanel variable to hold the header using the createHeader method
-
+		JPanel panel = new JPanel();
 		// 4. Add the header component to the quizPanel
-
+		panel.add(createHeader("Header"));
 		// 5. Add the quizPanel to the frame
-
+		frame.add(panel);
 		// 6. Use the createButton method to set the value of firstButton
-
+		JButton button1 = createButton("200");
 		// 7. Add the firstButton to the quizPanel
-
+		panel.add(button1);
 		// 8. Write the code to complete the createButton() method below. Check that your
 		// game looks like Figure 1 in the Jeopardy Handout - http://bit.ly/1bvnvd4.
 
@@ -92,14 +92,13 @@ public class Jeopardy implements ActionListener {
 	private JButton createButton(String dollarAmount) {
 		
 		// Create a new JButton
-
+		JButton button = new JButton();
 		// Set the text of the button to the dollarAmount
-
+		button.setText(dollarAmount);
 		// Increment the buttonCount (this should make the layout vertical)
-
+		buttonCount ++;
 		// Return your new button instead of the temporary button
-
-		return new JButton("temporary button");
+		return button;
 	}
 
 	@Override
